ad0e51300c1aed609cb1e27a858ec09e62945bc0
Marek Kollár <marek@wd02-0230.wd02-0230a.kn.vutbr.cz>
2024-04-15 12:23:25 +0200

    1

--- sorting.py
+++ sorting.py
@@ -1,5 +1,5 @@
 import os
-
+import csv
 
 def read_data(file_name):
     """
@@ -10,6 +10,28 @@ def read_data(file_name):
     """
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path, mode="r") as file:
+        reader = csv.DictReader(file)
+        data = {}
+        iter = 0
+        for row in reader:
+            for key, value in row.items():
+                if iter == 0:
+                    data[key] = [int(value)]
+                else:
+                    data[key].append(int(value))
+            iter = iter + 1
+    return data
+
+def selection_sort(zoznam):
+    for i in range(len(zoznam)):
+        idx = i
+        for j in range(i + 1, (len(zoznam))):
+            if zoznam[j] < zoznam[idx]:
+                idx = j
+        zoznam[i], zoznam[idx] = zoznam[idx], zoznam[i]
+    return zoznam
+
 
 
 def main():
@@ -17,4 +39,8 @@ def main():
 
 
 if __name__ == '__main__':
+    data = read_data('numbers.csv')
+    upratane = selection_sort(data['series_1'])
+    print(data)
+    print(upratane)
     main()
